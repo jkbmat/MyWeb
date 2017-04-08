8fa9f202d143a54456ae4f64cb57d8d1d555a5df
Jakub Matuška <jkb.matuska@gmail.com>
2017-04-08 10:54:09 +0200

    pinnedPic can now show target center

--- app/pinnedPic/index.js
+++ app/pinnedPic/index.js
@@ -9,18 +9,23 @@ require('./_style.scss');
 
 class PinnedPic extends React.Component {
   componentDidMount() {
+    this.resizeHandler = this.resizeHandler.bind(this);
+
+    this.targetSize = 30;
+
     const img = new Image();
     img.src = this.props.picture;
+
     let that = this;
 
     img.onload = () => {
       that.pictureW = img.naturalWidth;
       that.pictureH = img.naturalHeight;
 
-      window.addEventListener('resize', this.resizeHandler.bind(that));
+      window.addEventListener('resize', that.resizeHandler);
       that.hasMounted = true;
 
-      this.resizeHandler();
+      that.resizeHandler();
     };
   }
 
@@ -28,20 +33,24 @@ class PinnedPic extends React.Component {
     if (!this.hasMounted)
       return;
 
-    window.removeEventListener('resize', this.resizeHandler.bind(this));
+    window.removeEventListener('resize', this.resizeHandler);
+  }
+
+  componentDidUpdate() {
+    this.resizeHandler();
   }
 
   resizeHandler() {
-    const { centerX, centerY } = this.props;
+    const {centerX, centerY} = this.props;
 
     const containerW = this.container.scrollWidth;
     const containerH = this.container.scrollHeight;
 
     /*console.log("picture: ", this.pictureW, this.pictureH);
-    console.log("container: ", containerW, containerH);
-    console.log("center: ", centerX, centerY);*/
+     console.log("container: ", containerW, containerH);
+     console.log("center: ", centerX, centerY);*/
 
-    let calculatedCenterX = Math.max(
+    let calculatedX = Math.max(
       Math.min(
         -centerX + containerW / 2,
         0
@@ -49,7 +58,7 @@ class PinnedPic extends React.Component {
       -this.pictureW + containerW
     );
 
-    let calculatedCenterY = Math.max(
+    let calculatedY = Math.max(
       Math.min(
         -centerY + containerH / 2,
         0
@@ -58,25 +67,40 @@ class PinnedPic extends React.Component {
     );
 
     if (this.pictureW < containerW)
-      calculatedCenterX = containerW / 2 - this.pictureW / 2;
+      calculatedX = containerW / 2 - this.pictureW / 2;
 
     if (this.pictureH < containerH)
-      calculatedCenterY = containerH / 2 - this.pictureH / 2;
+      calculatedY = containerH / 2 - this.pictureH / 2;
 
-    this.container.style.backgroundPositionX = calculatedCenterX + 'px';
-    this.container.style.backgroundPositionY = calculatedCenterY + 'px';
+    this.container.style.backgroundPositionX = calculatedX + 'px';
+    this.container.style.backgroundPositionY = calculatedY + 'px';
+
+    const targetPos = {
+      x: calculatedX + centerX - this.targetSize / 2,
+      y: calculatedY + centerY - this.targetSize / 2
+    };
+
+    if(this.props.showTarget) {
+      this.target.style.left = targetPos.x + "px";
+      this.target.style.top = targetPos.y + "px";
+    }
   }
 
-  render () {
+  render() {
     return (
+      <div className="pinnedPic-wrapper">
+        {this.props.showTarget && <div className="pinnedPic-target" ref={(target) => this.target = target} />}
       <div
         className="pinnedPic"
         ref={(container) => this.container = container}
         style={{
-          background: 'url('+this.props.picture+')',
+          background: 'url(' + this.props.picture + ')',
           backgroundRepeat: 'no-repeat'
         }}
-      />
+      >
+
+      </div>
+      </div>
     );
   }
 }
@@ -85,7 +109,8 @@ export default connect(
   (state, ownProps) => ({
     centerX: ownProps.x,
     centerY: ownProps.y,
-    picture: ownProps.picture
+    picture: ownProps.picture,
+    showTarget: ownProps.showTarget
   }),
 
   (dispatch, ownProps) => ({})
